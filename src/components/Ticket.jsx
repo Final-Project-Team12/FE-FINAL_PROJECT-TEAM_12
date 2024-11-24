431d0daab3cef57f6758a5fa1951475f3b6bb789
TegarAlfaR <tegaralfa18052004@gmail.com>
2024-11-24 20:48:43 +0700

    feat: add header for fligth ticket page

---
+++ src/components/Ticket.jsx
@@ -0,0 +1,13 @@
+import Navbar from './UI/Navbar';
+import HeaderTicket from './UI/HeaderTicket';
+
+const Ticket = () => {
+  return (
+    <>
+      <Navbar />
+      <HeaderTicket />
+    </>
+  );
+};
+
+export default Ticket;
