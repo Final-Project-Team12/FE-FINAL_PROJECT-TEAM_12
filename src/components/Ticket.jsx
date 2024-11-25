03208821db0207f42e82160d22f73074f5cc340f
Melinda Wijaya <gmy4784@gmail.com>
2024-11-25 13:41:20 +0700

    feat: initialize LeftFilterTicket

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -1,11 +1,13 @@
 import Navbar from './UI/Navbar';
 import HeaderTicket from './UI/HeaderTicket';
+import LeftFilterTicket from "./UI/LeftFilterTicket";
 
 const Ticket = () => {
   return (
     <>
       <Navbar />
       <HeaderTicket />
+      <LeftFilterTicket/>
     </>
   );
 };
