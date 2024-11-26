f70d610943d24006ccb56bcf5236aeb1d8670a9f
Melinda Wijaya <gmy4784@gmail.com>
2024-11-26 22:30:01 +0700

    feat: add SortTicket at TicketPage

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -1,12 +1,14 @@
 import Navbar from './UI/Navbar';
 import HeaderTicket from './UI/HeaderTicket';
 import LeftFilterTicket from "./UI/LeftFilterTicket";
+import SortTicket from './UI/SortTicket';
 
 const Ticket = () => {
   return (
     <>
       <Navbar />
       <HeaderTicket />
+      <SortTicket />
       <LeftFilterTicket/>
     </>
   );
