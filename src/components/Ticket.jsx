e55372cc8cbbcb0709a2fea2b8d2e855a5e43113
yogiefani <yogiefaniyc@gmail.com>
2024-11-27 09:15:32 +0700

    test: in ticket

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -2,8 +2,6 @@ import Navbar from './UI/Navbar';
 import HeaderTicket from './UI/HeaderTicket';
 import LeftFilterTicket from './UI/LeftFilterTicket';
 import DetailsTicket from './UI/DetailsTicket';
-import SearchResultEmpty from './UI/SearchResultEmpety';
-import LoadingTicket from './UI/LoadingTicket';
 
 const Ticket = () => {
   return (
@@ -13,9 +11,7 @@ const Ticket = () => {
       <div>
         <div className="flex ml-[260px] mr-[212px]">
           <LeftFilterTicket />
-          <LoadingTicket />
-          {/* <SearchResultEmpty /> */}
-          {/* <DetailsTicket /> */}
+          <DetailsTicket />
         </div>
       </div>
     </>
