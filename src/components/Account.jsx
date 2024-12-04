e6f66390a18cb76dc5a4656bdc08aab22709277b
Melinda Wijaya <gmy4784@gmail.com>
2024-12-04 11:03:29 +0700

    feat: add NavbarLogin

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,9 +1,13 @@
 import AccountSidebar from "./UI/AccountSidebar";
+import NavbarLogin from "./UI/NavbarLogin";
 
 const Account = () => {
   return (
-    <div className="flex ml-[260px] mr-[212px]">
-      <AccountSidebar/>
+    <div className="min-h-screen bg-gray-50 overflow-x-hidden">
+      <NavbarLogin />
+      <div className="flex ml-[260px] mr-[212px]">
+        <AccountSidebar/>
+      </div>
     </div>
   );
 }
