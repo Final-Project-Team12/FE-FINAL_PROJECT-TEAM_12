9caa8cd7a1726b300a33e8a12c3d1eb41782e252
Melinda Wijaya <gmy4784@gmail.com>
2024-12-06 00:31:04 +0700

    perf: add navigate to AccountSidebar

--- src/components/UI/AccountSidebar.jsx
+++ src/components/UI/AccountSidebar.jsx
@@ -1,14 +1,19 @@
 import { LogOut, PencilLine, Settings } from 'lucide-react';
+import { useNavigate } from 'react-router-dom';
 
 const AccountSidebar = () => {
+  const navigate = useNavigate();
+
   return (
     <>
       <div className=" mb-[47px] mt-6">
         <div className="w-[260px] bg-white p-8">
-
           {/* Ubah Profil */}
           <div>
-            <div className="flex items-center justify-between cursor-pointer">
+            <div
+              className="flex items-center justify-between cursor-pointer group"
+              onClick={() => navigate('/account')}
+            >
               <div className="flex items-center gap-2">
                 <PencilLine size={24} className="text-purple-700" />
                 <span className="font-normal text-left">Ubah Profil</span>
@@ -19,7 +24,7 @@ const AccountSidebar = () => {
 
           {/* Pengaturan Akun */}
           <div>
-            <div className="flex items-center justify-between cursor-pointer">
+            <div className="flex items-center justify-between cursor-pointer group" onClick={() => navigate('/account')}>
               <div className="flex items-center gap-2">
                 <Settings size={24} className="text-purple-700" />
                 <span className="font-normal text-left">Pengaturan Akun</span>
@@ -28,19 +33,21 @@ const AccountSidebar = () => {
           </div>
           <hr className="my-4 bg-slate-400" />
 
-        {/* Logout */}
-        <div>
-          <div className="flex items-center justify-between cursor-pointer">
-            <div className="flex items-center gap-2">
-              <LogOut size={24} className="text-purple-700" />
-              <span className="font-normal text-left">Logout</span>
+          {/* Logout */}
+          <div>
+            <div className="flex items-center justify-between cursor-pointer">
+              <div className="flex items-center gap-2">
+                <LogOut size={24} className="text-purple-700" />
+                <span className="font-normal text-left">Logout</span>
+              </div>
             </div>
           </div>
-        </div>
-        <hr className="my-4 bg-slate-400" />
-        <div>
-          <footer className="text-xs font-normal text-slate-500 mb-6 text-center">Version 1.1.0</footer>
-        </div>
+          <hr className="my-4 bg-slate-400" />
+          <div>
+            <footer className="text-xs font-normal text-slate-500 mb-6 text-center">
+              Version 1.1.0
+            </footer>
+          </div>
         </div>
       </div>
     </>
