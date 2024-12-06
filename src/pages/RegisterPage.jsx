cb452e6f167b21b130dde6fd7e1a147adb2bbf26
TegarAlfaR <tegaralfa18052004@gmail.com>
2024-12-06 20:17:25 +0700

    feat: add ui for otp

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,17 +1,28 @@
-import RegisterForm from '../components/UI/RegisterForm';
-import AuthBanner from '../components/Elements/Banner/AuthBanner';
+import Otp from '../components/UI/OTP';
 
 const RegisterPage = () => {
   return (
-    <div className="flex">
-      <div className="w-1/2">
-        <AuthBanner />
-      </div>
-      <div className="w-1/2">
-        <RegisterForm />
-      </div>
+    <div>
+      <Otp />
     </div>
   );
 };
 
 export default RegisterPage;
+// import RegisterForm from '../components/UI/RegisterForm';
+// import AuthBanner from '../components/Elements/Banner/AuthBanner';
+
+// const RegisterPage = () => {
+//   return (
+//     <div className="flex">
+//       <div className="w-1/2">
+//         <AuthBanner />
+//       </div>
+//       <div className="w-1/2">
+//         <RegisterForm />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default RegisterPage;
