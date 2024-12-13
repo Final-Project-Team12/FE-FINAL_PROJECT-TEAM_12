9da07ffccaba91b96cf7a8b9af23eea3335cc797
Melinda Wijaya <gmy4784@gmail.com>
2024-12-13 18:41:30 +0700

    refactor: optimize notification component rendering and styling

--- src/components/UI/Notification.jsx
+++ src/components/UI/Notification.jsx
@@ -7,7 +7,24 @@ import {
   sortFlightsByDate,
   filterFlightsByDateRange,
 } from '../../utils/dateUtils';
-import { Bell, PencilLine } from 'lucide-react';
+import { Bell } from 'lucide-react';
+
+const notif = [
+  {
+    type: "Promosi",
+    title: "Dapatkan Potongan 50% Tiket!",
+    description: "Syarat dan Ketentuan berlaku!",
+    date: "14 Maret 2020",
+    time: "14:04"
+  },
+  {
+    type: "Pemberitahuan",
+    title: "Terdapat perubahan pada jadwal penerbangan kode booking 45GT6",
+    description: "Cek jadwal perjalanan Anda disini!",
+    date: "14 Maret 2020",
+    time: "14:04"
+  }
+];
 
 const Notification = () => {
   const [selectedCardId, setSelectedCardId] = useState(null);
@@ -35,36 +52,39 @@ const Notification = () => {
     );
   };
 
+  const getDotColor = (type) => {
+    return type === "Promosi" ? "bg-green-600" : "bg-red-600";
+  };
+
   return (
     <>
       <Navbar />
       <HeaderHistory onDateRangeChange={handleDateRangeChange} />
-      <div className="flex-col lg:flex-row gap-12 px-[260px] py-4 min-h-[calc(100vh-84px)]">
-        <div className="flex p-4 sm:p-6 space-x-4">
-          <div>
-            <Bell className="bg-purple-400 w-6 h-6 text-white rounded-full p-1 mt-2"/>
-          </div>
-          
-          <div>
-            <span className="text-slate-500 text-sm">Promosi</span>
-            <h3>Dapatkan Potongan 50% Tiket!</h3>
-            <p className="text-slate-500 text-sm">Syarat dan Ketentuan berlaku!</p>
-          </div>
-        </div>
-        <hr className="my-4 bg-slate-400" />
+      <div className="w-full flex-col lg:flex-row gap-12 px-[280px] py-4 min-h-[calc(100vh-84px)] mt-3">
+        {notif.map((notif, index) => (
+          <React.Fragment key={index}>
+            <div className="flex w-full space-x-4">
+              <div>
+                <Bell className="bg-purple-400 w-6 h-6 text-white rounded-full p-1 mt-1" />
+              </div>
 
-        <div className="flex p-4 sm:p-6 space-x-4">
-          <div>
-            <Bell className="bg-purple-400 w-6 h-6 text-white rounded-full p-1 mt-2"/>
-          </div>
-          
-          <div>
-            <span className="text-slate-500 text-sm">Promosi</span>
-            <h3>Dapatkan Potongan 50% Tiket!</h3>
-            <p className="text-slate-500 text-sm">Syarat dan Ketentuan berlaku!</p>
-          </div>
-          <hr className="my-4 bg-slate-400" />
-        </div>
+              <div className="w-full">
+                <div className="flex justify-between w-full">
+                  <span className="text-slate-500 text-sm">{notif.type}</span>
+                  <div className="flex items-center space-x-2">
+                    <span className="text-slate-500 text-sm">{notif.date}, {notif.time} </span>
+                    <div className={`rounded-full w-2 h-2 ${getDotColor(notif.type)}`}></div>
+                  </div>
+                </div>
+                <h3>{notif.title}</h3>
+                <p className="text-slate-500 text-sm">
+                  {notif.description}
+                </p>
+              </div>
+            </div>
+            {index < notif.length - 1 && <hr className="my-4 bg-slate-400" />}
+          </React.Fragment>
+        ))}
       </div>
     </>
   );
