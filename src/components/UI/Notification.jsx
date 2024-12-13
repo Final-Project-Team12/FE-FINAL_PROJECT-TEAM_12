c60717d72040fbb4b557e8434b0cccc98cca9ab0
Melinda Wijaya <gmy4784@gmail.com>
2024-12-13 18:47:06 +0700

    refactor: improve code formatting and indentation

--- src/components/UI/Notification.jsx
+++ src/components/UI/Notification.jsx
@@ -62,29 +62,27 @@ const Notification = () => {
       <HeaderHistory onDateRangeChange={handleDateRangeChange} />
       <div className="w-full flex-col lg:flex-row gap-12 px-[280px] py-4 min-h-[calc(100vh-84px)] mt-3">
         {notif.map((notif, index) => (
-          <React.Fragment key={index}>
-            <div className="flex w-full space-x-4">
-              <div>
-                <Bell className="bg-purple-400 w-6 h-6 text-white rounded-full p-1 mt-1" />
-              </div>
-
-              <div className="w-full">
-                <div className="flex justify-between w-full">
-                  <span className="text-slate-500 text-sm">{notif.type}</span>
-                  <div className="flex items-center space-x-2">
-                    <span className="text-slate-500 text-sm">{notif.date}, {notif.time} </span>
-                    <div className={`rounded-full w-2 h-2 ${getDotColor(notif.type)}`}></div>
-                  </div>
-                </div>
-                <h3>{notif.title}</h3>
-                <p className="text-slate-500 text-sm">
-                  {notif.description}
-                </p>
-              </div>
+          <><div key={index} className="flex w-full space-x-4">
+            <div>
+              <Bell className="bg-purple-400 w-6 h-6 text-white rounded-full p-1 mt-1" />
             </div>
-            {index < notif.length - 1 && <hr className="my-4 bg-slate-400" />}
-          </React.Fragment>
-        ))}
+
+            <div className="w-full">
+              <div className="flex justify-between w-full">
+                <span className="text-slate-500 text-sm">{notif.type}</span>
+                <div  className="flex items-center space-x-2">
+                  <span className="text-slate-500 text-sm">{notif.date}, {notif.time} </span>
+                  <div className={`rounded-full w-2 h-2 ${getDotColor(notif.type)}`}></div>
+                </div>
+                
+              </div>
+              <h3>{notif.title}</h3>
+              <p className="text-slate-500 text-sm">
+                {notif.description}
+              </p>
+            </div>
+          </div><hr className="my-4 bg-slate-400" /></>
+        ))};
       </div>
     </>
   );
