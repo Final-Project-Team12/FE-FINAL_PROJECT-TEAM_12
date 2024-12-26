31c00cc49e5c309daeca351e62ec0c8bc779f78a
Rafly Aziz Abdillah <raflyazizabdillah30@gmail.com>
2024-12-26 10:48:53 +0700

    add: add loading

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,9 +1,18 @@
 import AuthBanner from '../components/Elements/Banner/AuthBanner';
 import RegisterForm from '../components/UI/RegisterForm';
+import Loading from '../components/Elements/Loading/Loading';
+import { useSelector } from 'react-redux';
 
 const RegisterPage = () => {
+  const loading = useSelector((state) => state.register.loading);
+
   return (
-    <div className="flex min-h-screen bg-white">
+    <div className="relative flex min-h-screen bg-white">
+      {loading && (
+        <div className="absolute inset-0 bg-black/20 backdrop-blur-[2px] z-50 flex items-center justify-center">
+          <Loading />
+        </div>
+      )}
       <div className="hidden md:flex md:w-1/2">
         <AuthBanner />
       </div>
